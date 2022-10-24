6068ccf259b730f291dd4d1bfbaa518c8641a353
eaccipe <eaccipe@gmail.com>
2022-10-24 14:16:26 +0200

    create offer form

--- app/symfony/src/Controller/OffersController.php
+++ app/symfony/src/Controller/OffersController.php
@@ -21,6 +21,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
 
+
 #[Route('/offers')]
 class OffersController extends AbstractController {
 
