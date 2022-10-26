9d786991621b638f7ba7a66cabb754928643242c
eaccipe <eaccipe@gmail.com>
2022-10-26 16:28:27 +0200

    redirect home

--- app/symfony/src/Controller/HomeController.php
+++ app/symfony/src/Controller/HomeController.php
@@ -11,11 +11,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        $path = $this->getParameter('kernel.project_dir') . '/public/assets/08-Memo-Global_compressed.pdf';
-//        $files = new File($path);
-//        dd($files);
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+      return $this->redirectToRoute('app_offers_all');
     }
 }
